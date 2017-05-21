4c8e11a0cdad5e94a2bf340aca0e98ccd7451eb6
johanFornari <johandebonaire@gmail.com>
2017-05-21 14:39:33 -0500

    se agrego un estilo y una funcion

---
+++ holamundo.js
@@ -0,0 +1,7 @@
+function ejecutar() {
+
+var palabra = document.getElementById("palabra").value.toString();
+
+document.getElementById("palabras").innerHTML = "La palabra ingresada es:  "+ palabra;
+
+}

44a5bf50e3e7d79af050ddeb76b9edad5e112e0b
johanFornari <johandebonaire@gmail.com>
2017-05-21 15:48:48 -0500

    se creo el boton de limpiar

--- holamundo.js
+++ holamundo.js
@@ -5,3 +5,12 @@ var palabra = document.getElementById("valor").value.toString();
 document.getElementById("palabras").innerHTML = "La palabra ingresada es:  "+ palabra;
 
 }
+
+function limpiar() {
+
+
+
+document.getElementById("palabras").innerHTML = "";
+document.getElementById("valor").value = "";
+
+}
